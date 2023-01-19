e11f27320b1c153a3d2edbc7e9287491d8b5eb0f
ZackCgl <100427794+ZackCgl@users.noreply.github.com>
2023-01-19 02:59:07 +0100

    add padding

--- src/pages/admin/dashboard.tsx
+++ src/pages/admin/dashboard.tsx
@@ -17,7 +17,7 @@ function dashboard() {
         </p>
         <div className="mt-10 flex gap-2">
           <Link href="/admin/addBlogNL">
-            <button className="mr-2 mb-2 rounded-lg bg-blue-700 px-5 py-2.5 text-sm font-medium text-white hover:bg-blue-800 focus:outline-none focus:ring-4 focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+            <button className="mr-2 ml-2 mb-2 rounded-lg bg-blue-700 px-5 py-2.5 text-sm font-medium text-white hover:bg-blue-800 focus:outline-none focus:ring-4 focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
               Maak blog NL
             </button>
           </Link>
