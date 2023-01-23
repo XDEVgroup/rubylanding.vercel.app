78be4f5e1408704708bbc0669a7bccf9553d730d
ZackCgl <100427794+ZackCgl@users.noreply.github.com>
2023-01-23 14:48:38 +0100

    add

--- src/pages/admin/dashboard.tsx
+++ src/pages/admin/dashboard.tsx
@@ -18,12 +18,12 @@ function dashboard() {
         <div className="mt-10 flex gap-2">
           <Link href="/admin/addBlogNL">
             <button className="mr-2 ml-2 mb-2 rounded-lg bg-blue-700 px-5 py-2.5 text-sm font-medium text-white hover:bg-blue-800 focus:outline-none focus:ring-4 focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
-              Blog NL (beta)
+              Blog NL
             </button>
           </Link>
           <Link href="/admin/addBlogEN">
             <button className="mr-2 mb-2 rounded-lg bg-blue-700 px-5 py-2.5 text-sm font-medium text-white hover:bg-blue-800 focus:outline-none focus:ring-4 focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
-              Blog EN (beta)
+              Blog EN
             </button>
           </Link>
           <Link href="/admin/aanvragen">
