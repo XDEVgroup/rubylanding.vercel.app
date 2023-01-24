67c899b65ca2949fff075af330a5ae0a4db0ecb9
ZackCgl <100427794+ZackCgl@users.noreply.github.com>
2023-01-24 20:32:06 +0100

    add

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -3,22 +3,7 @@ module.exports = {
   content: ["./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {
-      animation: {
-        tilt: 'tilt 10s infinite linear',
-      },
-      keyframes: {
-        tilt: {
-          '0%, 50%, 100%': {
-            transform: 'rotate(0deg)',
-          },
-          '25%': {
-            transform: 'rotate(0.5deg)',
-          },
-          '75%': {
-            transform: 'rotate(-0.5deg)',
-          },
-        },
-      },
+      
     },
   },
   plugins: [],
