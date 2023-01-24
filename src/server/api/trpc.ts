1c3cb8a84b424451dd0068e1b715dfb4dfff31e2
ZackCgl <100427794+ZackCgl@users.noreply.github.com>
2023-01-24 22:26:37 +0100

    export const nodejs

--- src/server/api/trpc.ts
+++ src/server/api/trpc.ts
@@ -122,3 +122,7 @@ const enforceUserIsAuthed = t.middleware(({ ctx, next }) => {
  * @see https://trpc.io/docs/procedures
  */
 export const protectedProcedure = t.procedure.use(enforceUserIsAuthed);
+
+export const config = {
+  runtime: "nodejs",
+}
