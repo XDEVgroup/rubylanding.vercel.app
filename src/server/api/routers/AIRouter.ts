352f6f692a7851e8a43e219c0ce6aa6a1c8f8bf7
ZackCgl <100427794+ZackCgl@users.noreply.github.com>
2023-01-25 00:11:29 +0100

    300

--- src/server/api/routers/AIRouter.ts
+++ src/server/api/routers/AIRouter.ts
@@ -34,7 +34,7 @@ export const AIRouter = createTRPCRouter({
         model: 'text-davinci-003',
         prompt: `Maak een blog met de volgende titel: ${input.topic}`,
         temperature: 1,
-        max_tokens: 400,
+        max_tokens: 300,
         top_p: 1,
         frequency_penalty: 0,
         presence_penalty: 0,
