19ce41ccb605e8415a8d5fd70c514bbe54dd5690
ZackCgl <100427794+ZackCgl@users.noreply.github.com>
2023-01-26 22:04:59 +0100

    removed unused js

--- src/pages/blogs/index.tsx
+++ src/pages/blogs/index.tsx
@@ -1,16 +1,11 @@
-import { useRouter } from "next/router";
 import Header from "../../components/HeaderBlack";
 import Image from "next/image";
 import Footer from "../../components/Footer";
-import { api } from "../../utils/api";
 import Head from "next/head";
 import Whatsapp from "../../components/Whatsapp";
 import BlogAllIndex from "../../components/BlogAllIndex";
 
 function BlogId() {
-  const router = useRouter();
-  //focus deze query voor serverfetch
-
   return (
     <>
       <Head>
